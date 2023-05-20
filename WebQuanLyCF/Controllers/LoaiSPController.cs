2a85f16d9383c8da2720349b4bac5dce83993865
NguyenHongBan <103940809+NguyenHongBan@users.noreply.github.com>
2023-05-20 16:40:27 +0700

    20/5

---
+++ WebQuanLyCF/Controllers/LoaiSPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebQuanLyCF.Controllers
+{
+    public class LoaiSPController : Controller
+    {
+        // GET: LoaiSP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
